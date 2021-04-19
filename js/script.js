b909fdc94adfa6405c2e0761b25fe9d3d2c6ee57
DociIva <doci.iva@hotmail.com>
2021-04-19 17:48:14 +0200

    add icon html

--- js/script.js
+++ js/script.js
@@ -100,5 +100,53 @@ const icons = [
         type: 'user',
         family: 'fas',
     },
-
 ];
+
+// Icon container
+// querySelector e per il 1 elemento che incontra
+const container = document.querySelector('.icons');
+console.log(container);
+// 1. situazione printare le icone a schermo
+// facciamo una funzione | abbiamo così (le icone stesse, e dove metterle)
+
+printIcons(icons, container);
+
+
+
+
+
+
+/*********************************
+ * Functions
+ ********************************/
+
+/**
+ * print icons pn screen (sono parametri)
+ */
+function printIcons(icons, container) {
+    //console.log(icons);
+    //Generare il markup per le icone| let perchè si deve sovrascrivere ogni volta
+    let  html = '';
+    icons.forEach( (icon) => {
+       // destru
+       const {family, prefix, name} = icon;
+
+       html +=
+       // qua ti porti ul markup, ora lo cambi , interpolazione
+       `<div class="icon p-20">
+                <i class="${family} ${prefix}${name}" 
+                    style="color:#333"></i>
+                <div class="title">${name}</div>
+        </div>`;
+    });
+    console.log(html);
+    
+    // mettere il contenuto
+    container.innerHTML = html;
+    
+}
+
+
+
+
+
